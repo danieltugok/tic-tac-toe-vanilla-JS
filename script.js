1d890b9c0384b5cc7e8491eec1d332ca850fca6c
Daniel <daniel.kogut@afilio.com.br>
2021-08-12 22:36:07 -0300

    finalizing the game

--- script.js
+++ script.js
@@ -11,19 +11,28 @@ let playing = false;
 
 reset();
 
-
 // Events
 document.querySelector('.reset').addEventListener('click', reset);
-
-
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick);
+})
 
 
 // Functions
+function itemClick ( event ){
+    let item = event.target.getAttribute('data-item');
+    if (board[item] === '' && playing) {
+        board[item] = turnPlayer;
+        renderBoard();
+        togglePlayer();
+    }
+}
+
 function reset () {
     warning = '';
 
     let random = Math.floor(Math.random() * 2);
-    player = (random === 0) ? 'x' : 'o';
+    turnPlayer = (random === 0) ? 'x' : 'o';
 
     for(let i in board) {
         board[i] = '';
@@ -33,21 +42,73 @@ function reset () {
 
     renderBoard();
     renderInfo();
-
 }
 
 function renderBoard(){
     for(let i in board) {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = board[i];
+    }
 
+    checkGame();
+}
+
+function checkGame () {
+    if (checkWinnerFor('x')) {
+        warning = 'O "x" venceu'
+        playing = false;
+    } else if (checkWinnerFor('0')) {
+        warning = 'O "o" venceu'
+        playing = false;
+    } else if (isFull()){
+        warning = 'Deu empate'
+        playing = false;
     }
 }
 
+function checkWinnerFor(player){
+    let possibilities = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for( let w in possibilities) {
+        let pArray = possibilities[w].split(','); // a1,a2,a3
+        let hasWon = pArray.every(option => board[option] == player);
+        if (hasWon) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isFull(){
+    for(let i in board){
+        if (board[i] === '') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function renderInfo() {
-    document.querySelector('.vez').innerHTML = player;
-    document.querySelector('.resultado').innerHTML = warning;
-    
+    document.querySelector('.vez').innerHTML = turnPlayer;
+    document.querySelector('.resultado').innerHTML = warning; 
+}
+
+function togglePlayer(){
+    turnPlayer = (turnPlayer === 'x') ? 'o' : 'x';
+    renderInfo();
 }
 
 
